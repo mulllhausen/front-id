207d58051ca4ab1526ebb2ca9b4923872266367a
peter <petermiller1986@gmail.com>
2021-05-10 09:41:29 +0930

    ok fine. make the static site builder reeeeeeeeeally flexible, since github pages requires the site under root dir

--- .source/config.sh
+++ .source/config.sh
@@ -6,9 +6,13 @@
 domain="frontid.null.place"
 author="mulllhausen"
 site_description="the frontend id system"
-processing_dir="processing"
-production_dir="production"
+source_dir="."
+processing_dir="../processing"
+production_dir="../"
 tmp_err_log="build-site-err.txt"
 
 # left first, right last
 process_file_extensions="css html"
+
+# no special order
+site_file_extensions="html css js svg ico png"
