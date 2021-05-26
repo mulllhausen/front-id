f24253aa68441f9df63177a99e80135be715d631
peter <petermiller1986@gmail.com>
2021-05-27 00:30:28 +0930

    fix file path

--- .source/js/main.js
+++ .source/js/main.js
@@ -2,11 +2,11 @@
 <?php
 
 if (!defined("processing_dir")) define("processing_dir", $argv[1]);
-include_once(processing_dir."/config.php");
-include_once(processing_dir."/functions.php");
+require_once(processing_dir."/config.php");
+require_once(processing_dir."/functions.php");
 
-include_once_and_explain(processing_dir."/js/components/utils.js");
-include_once_and_explain(processing_dir."/js/components/popup_modal.js");
-include_once_and_explain(processing_dir."/js/components/hexagon_profile_pic.js");
+require_once_and_explain(processing_dir."/js/components/utils.js");
+require_once_and_explain(processing_dir."/js/components/popup_modal.js");
+require_once_and_explain(processing_dir."/js/components/edit_profile_pic.js");
 
 ?>
