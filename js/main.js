7775325e1290e1f5b669f79d2aa7456297167014
peter <petermiller1986@gmail.com>
2021-06-01 23:01:23 +0930

    publish

--- js/main.js
+++ js/main.js
@@ -44,17 +44,28 @@ function triggerEvent(element, type) {
         else el.fireEvent('on' + type);
     }
 }
+function toLocalStorage(k,v) {
+    localStorage.setItem(k, JSON.stringify(v));
+}
+function fromLocalStorage(k) {
+    return JSON.parse(localStorage.getItem(k));
+}
 
 /* popup_modal ****************************************************************/
 
 function showPopup(contentID) {
-    var contentElCopy = document.getElementById(contentID).cloneNode(true);
-    document.getElementById('popupModal').appendChild(contentElCopy);
+    var contentEl = document.getElementById(contentID);
+    var contentHTML = contentEl.outerHTML;
+    contentEl.parentNode.removeChild(contentEl);
+    document.getElementById('popupModal').innerHTML = contentHTML;
     document.getElementById('popupModalBackground').style.display = 'inline-block';
 }
 
 function hidePopup() {
-    document.getElementById('popupModal').innerHTML = '';
+    var popup = document.getElementById('popupModal');
+    var contentElCopy = popup.children[0].cloneNode(true);
+    document.getElementById('modalHoldings').appendChild(contentElCopy);
+    popup.innerHTML = '';
     document.getElementById('popupModalBackground').style.display = 'none';
 }
 addEvent(document, 'ready', function () {
@@ -66,17 +77,132 @@ addEvent(document, 'ready', function () {
 
 /* edit_profile_pic ***********************************************************/
 
+var tmpUploadedImageBase64 = '';
+addEvent(document, 'ready', function () {
+    addEvent(
+        document.getElementById('hexagonProfilePic'),
+        'load',
+        addSVGClickEvent
+    );
+});
+
+function addSVGClickEvent() {
+    var svgDoc = document.getElementById('hexagonProfilePic').contentDocument;
+    if (svgDoc == null) return;
+    var clickArea = svgDoc.getElementById('profilePicClicker');
+    addEvent(clickArea, 'click', popupEditProfilePic);
+}
+
 function popupEditProfilePic() {
     showPopup('editProfilePic');
-    document.getElementById('editProfilePicUpload').innerHTML =
-    'click here to upload or drag and drop';
+    var img = document.getElementById('editProfilePicUploadedImg');
+    var profile = fromLocalStorage('profile');
+    if (profile != null && profile.hasOwnProperty('picB64')) {
+        img.src = profile.picB64;
+    }
+    addEvent(img, 'load', freeImgMemory);
+    addEvent(
+        document.getElementById('editProfilePicUpload'),
+        'change',
+        picUploadHandler
+    );
+    addEvent(
+        document.getElementById('editProfilePicChange'),
+        'click',
+        changePicHandler
+    );
+    addEvent(
+        document.getElementById('editProfilePicCrop'),
+        'click',
+        cropPicHandler
+    );
+    addEvent(
+        document.getElementById('editProfilePicDone'),
+        'click',
+        donePicHandler
+    );
+    addEvent(
+        document.getElementById('editProfilePicCancel'),
+        'click',
+        cancelPicHandler
+    );
 }
-addEvent(document, 'ready', function () {
-    var hexagonSVGEl = document.getElementById('hexagonProfilePic');
-    addEvent(hexagonSVGEl, 'load', function () {
-        var svgDoc = hexagonSVGEl.contentDocument;
-        if (svgDoc == null) return;
-        var clickArea = svgDoc.getElementById('profilePicClicker');
-        addEvent(clickArea, 'click', popupEditProfilePic);
+
+function cancelPicHandler() {
+    tmpUploadedImageBase64 = '';    hidePopup();
+}
+function changePicHandler() {
+}
+function cropPicHandler() {
+}
+function donePicHandler() {
+    if (tmpUploadedImageBase64 == '') return hidePopup();
+
+    var profile = fromLocalStorage('profile');
+    if (profile == null) profile = {};    profile.picB64 = tmpUploadedImageBase64;
+    toLocalStorage('profile', profile);
+    hidePopup();
+}
+
+function freeImgMemory() {
+    URL.revokeObjectURL(this.src);
+}
+
+function picUploadHandler() {
+    if (!this.files) return displayUploadedImg('hide');
+    if (this.files.length == 0) return displayUploadedImg('hide');
+    var uploadedFile = this.files[0];
+
+    file2Base64(uploadedFile).then(function (b64) {
+        document.getElementById('editProfilePicUploadedImg').src = b64;
+        displayUploadedImg('show');
+        tmpUploadedImageBase64 = b64; 
+        toggleEditMode('crop');
     });
-});
+}
+
+function file2Base64(file) {
+    return new Promise(function (resolve, reject) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            resolve(reader.result);
+        };
+        reader.onerror = function (error) {
+            reject(error);
+        };
+    });
+}
+
+function displayUploadedImg(status) {
+    var svg = document.getElementById('editProfilePicDude');
+    var img = document.getElementById('editProfilePicUploadedImg');
+    var label = document.querySelector('#editProfilePicUpload + label');
+    switch (status) {
+        case 'show':
+            img.style.display = 'block';
+            svg.style.display = 'none';
+            label.style.display = 'none';
+            break;
+        case 'hide':
+            img.style.display = 'none';
+            svg.style.display = 'inline-block';
+            label.style.display = 'block';
+            break;
+    }
+}
+
+function toggleEditMode(mode) {
+    var btnChange = document.getElementById('editProfilePicChange');
+    var btnCrop = document.getElementById('editProfilePicCrop');
+    switch (mode) {
+        case 'change':
+            btnCrop.style.display = 'none';
+            btnChange.style.display = 'inline-block';
+            break;
+        case 'crop':
+            btnCrop.style.display = 'inline-block';
+            btnChange.style.display = 'none';
+            break;
+    }
+}
